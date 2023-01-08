1e1b1d5d5a2ad2410a309a0106930021e268a9ab
Caleb Cox <canac@users.noreply.github.com>
2023-01-08 15:21:50 -0600

    Add import route

--- main.ts
+++ main.ts
@@ -1,22 +1,49 @@
+import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
 import { readLines } from "https://deno.land/std@0.171.0/io/mod.ts";
+import { readerFromStreamReader } from "https://deno.land/std@0.171.0/streams/mod.ts";
+import { Hono } from "https://deno.land/x/hono@v3.0.0-rc.4/mod.ts";
+import { z } from "https://deno.land/x/zod@v3.20.2/mod.ts";
 import { Database } from "./db.ts";
 import { parseMessage, readMessages } from "./parse.ts";
 
-const mboxFilename = Deno.args[0];
-if (!mboxFilename) {
-  console.log("Missing MBOX filename argument!");
-  Deno.exit(1);
-}
+const envSchema = z.object({
+  PORT: z.coerce.number().optional(),
+  POSTGRES_URL: z.string(),
+});
+const env = envSchema.parse({
+  PORT: Deno.env.get("PORT"),
+  POSTGRES_URL: Deno.env.get("POSTGRES_URL"),
+});
 
-const postgresUrl = Deno.env.get("POSTGRES_URL");
-if (!postgresUrl) {
-  throw new Error("$POSTGRES_URL environment variable is missing!");
-}
+const db = new Database(env.POSTGRES_URL);
 
-const db = new Database(postgresUrl);
-await db.truncateMessages();
+const app = new Hono();
+app.get("/", async (c) => {
+});
+app.post("/import", async (c) => {
+  const form = await c.req.formData();
+  const mbox = form.get("mailbox");
+  if (!mbox || typeof mbox === "string") {
+    return c.text("mailbox upload is missing or is not a file", 500);
+  }
 
-const mboxFile = await Deno.open(mboxFilename);
-for await (const message of readMessages(readLines(mboxFile))) {
-  db.insertMessage(parseMessage(message));
-}
+  const reader = readerFromStreamReader(mbox.stream().getReader());
+  const messages = readMessages(readLines(reader));
+
+  await db.truncateMessages();
+
+  let count = 0;
+  let errors = 0;
+  for await (const rawMessage of messages) {
+    try {
+      db.insertMessage(parseMessage(rawMessage));
+      ++count;
+    } catch (err) {
+      console.error(err);
+      ++errors;
+    }
+  }
+
+  return c.text(`Imported ${count} emails with ${errors} errors`);
+});
+serve(app.fetch, { port: env.PORT });
